16e05d560d8debf39c2623c15d79534b7827178f
Steve Travis <stevetravis11@gmail.com>
2019-04-05 13:47:03 -0400

    wip: Added moment.js library and link.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,18 @@
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDfnDvYeXayH-RrGcNSIzsFHX-CajLlkqQ",
-    authDomain: "gtcbc-trainscheduler.firebaseapp.com",
-    databaseURL: "https://gtcbc-trainscheduler.firebaseio.com",
-    projectId: "gtcbc-trainscheduler",
-    storageBucket: "",
-    messagingSenderId: "225133807135"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDfnDvYeXayH-RrGcNSIzsFHX-CajLlkqQ",
+  authDomain: "gtcbc-trainscheduler.firebaseapp.com",
+  databaseURL: "https://gtcbc-trainscheduler.firebaseio.com",
+  projectId: "gtcbc-trainscheduler",
+  storageBucket: "",
+  messagingSenderId: "225133807135"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  //Variables
+// Variables
 var trainName = "";
 var trainDestination = "";
 var firstTrain = "";
@@ -21,38 +21,39 @@ var nextTrain = null;
 var firstConvertedTime = null;
 var minutesNextTrain = null;
 
-//Form onlcick function
-$("#saveButton").on("click", function(){
+// Form onlcick function
+$("#saveButton").on("click", function () {
   //prevent default behavior
-  event.preventDefault();  
+  event.preventDefault();
 
-//Get user input from form and store in variables
-trainName = $("#trainName").val().trim();
-destination =  $("#destination").val().trim();
-firstTrain =  $("#firstTrain").val().trim();
-frequency =  $("#frequencyMins").val().trim();
+  // Get user input from form and store in variables
+  trainName = $("#trainName").val().trim();
+  destination = $("#destination").val().trim();
+  firstTrain = $("#firstTrain").val().trim();
+  frequency = $("#frequencyMins").val().trim();
 
-console.log("Train Name: " +trainName);
-console.log("Destination: " +destination);
-console.log("First Train: " +firstTrain);
-console.log("Train Frequency: " +frequency);
+  console.log("Train Name: " + trainName);
+  console.log("Destination: " + destination);
+  console.log("First Train: " + firstTrain);
+  console.log("Train Frequency: " + frequency);
 
-// Creates variables to connect to firebase
-var trainInfo = {
-  trainName: trainName,
-  destination: destination,
-  firstTrain: firstTrain,
-  frequency: frequency
-};
+  // Creates variables to connect to firebase
+  var trainInfo = {
+    trainName: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+  };
 
-//push trainInfo to database
-database.ref().set(trainInfo);
+  // Push trainInfo to database
+  database.ref().set(trainInfo);
 
-//Clean input fields
-clearForm()
+  // Clean input fields
+  clearForm()
 
 });
 
+// Function to clear submission fields
 function clearForm() {
   $("#trainName").val("");
   $("#destination").val("");
@@ -60,6 +61,21 @@ function clearForm() {
   $("#frequency").val("");
 };
 
-database.ref().on("child_added", function(childSnapshot) {
+database.ref().on("child_added", function (childSnapshot) {
   console.log(childSnapshot.val());
-}
+
+  // Store firebase data into variables.
+  trainName = childSnapshot.val().trainName;
+  destination = childSnapshot.val().destination;
+  firstTrain = childSnapshot.val().firstTrain;
+  frequency = childSnapshot.val().frequency;
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
+
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  
+});
